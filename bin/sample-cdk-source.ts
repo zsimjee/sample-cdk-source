8f4091a431588f5a3a066afaf0b01e5f0a2f72bc
Caleb Courier <caleb.courier@gmail.com>
2022-08-31 12:57:14 -0500

    try using profile, log env vars

--- bin/sample-cdk-source.ts
+++ bin/sample-cdk-source.ts
@@ -3,12 +3,18 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SampleCdkSourceStack } from '../lib/sample-cdk-source-stack';
 
-console.info(`Launching Stack in account ${process.env.CDK_DEFAULT_ACCOUNT} and region ${process.env.CDK_DEFAULT_REGION}.`)
+console.info(`AWS_PROFILE: ${process.env.AWS_PROFILE}`);
+console.info(`AWS_ACCOUNT_ID: ${process.env.AWS_ACCOUNT_ID}`);
+console.info(`AWS_REGION: ${process.env.AWS_REGION}`);
+console.info(`CDK_DEFAULT_ACCOUNT: ${process.env.CDK_DEFAULT_ACCOUNT}`);
+console.info(`CDK_DEFAULT_REGION: ${process.env.CDK_DEFAULT_REGION}`);
 
 const app = new cdk.App();
-new SampleCdkSourceStack(app, 'SampleCdkSourceStack', {
-  env: { 
-    account: process.env.CDK_DEFAULT_ACCOUNT, 
-    region: process.env.CDK_DEFAULT_REGION
-  }
-});
+new SampleCdkSourceStack(app, 'SampleCdkSourceStack');
+
+// , {
+//   env: { 
+//     account: process.env.CDK_DEFAULT_ACCOUNT, 
+//     region: process.env.CDK_DEFAULT_REGION
+//   }
+// }
