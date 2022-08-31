41cf6320851b2376d9af402311d7c015d0ac58ba
Caleb Courier <caleb.courier@gmail.com>
2022-08-31 09:19:30 -0500

    log env information

--- bin/sample-cdk-source.ts
+++ bin/sample-cdk-source.ts
@@ -3,10 +3,12 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SampleCdkSourceStack } from '../lib/sample-cdk-source-stack';
 
+console.info(`Launching Stack in account ${process.env.CDK_DEFAULT_ACCOUNT} and region ${process.env.CDK_DEFAULT_REGION}.`)
+
 const app = new cdk.App();
 new SampleCdkSourceStack(app, 'SampleCdkSourceStack', {
   env: { 
     account: process.env.CDK_DEFAULT_ACCOUNT, 
-    region: process.env.CDK_DEFAULT_REGION 
+    region: process.env.CDK_DEFAULT_REGION
   }
 });
