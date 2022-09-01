d53147262767320e381d4f1899d4e595995e3291
Caleb Courier <caleb.courier@gmail.com>
2022-09-01 10:33:15 -0500

    update from CfnParams to Env Vars

--- bin/sample-cdk-source.ts
+++ bin/sample-cdk-source.ts
@@ -3,16 +3,15 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SampleCdkSourceStack } from '../lib/sample-cdk-source-stack';
 
-console.info(`AWS_PROFILE: ${process.env.AWS_PROFILE}`);
-console.info(`AWS_ACCOUNT_ID: ${process.env.AWS_ACCOUNT_ID}`);
-console.info(`AWS_REGION: ${process.env.AWS_REGION}`);
-console.info(`CDK_DEFAULT_ACCOUNT: ${process.env.CDK_DEFAULT_ACCOUNT}`);
-console.info(`CDK_DEFAULT_REGION: ${process.env.CDK_DEFAULT_REGION}`);
-
 const stackName = process.env.STACK_NAME || 'SampleCdkSourceStack';
+const queueName = process.env.QueueName!;
+const visibilityTimeout = Number(process.env.VisibilityTimeout || 30);
 
 const app = new cdk.App();
 new SampleCdkSourceStack(app, stackName, {
+  queueName,
+  visibilityTimeout
+}, {
   env: { 
     account: process.env.CDK_DEFAULT_ACCOUNT, 
     region: process.env.CDK_DEFAULT_REGION
--- lib/sample-cdk-source-stack.ts
+++ lib/sample-cdk-source-stack.ts
@@ -1,25 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { CfnParameter } from 'aws-cdk-lib';
+
+export interface SampleCdkSourceStackProps {
+  queueName: string;
+  visibilityTimeout: number;
+}
 
 export class SampleCdkSourceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: Construct, id: string, props: SampleCdkSourceStackProps, stackProps?: cdk.StackProps) {
+    super(scope, id, stackProps);
 
-    const queueName = new CfnParameter(this, 'QueueName', {
-      description: 'The name for the queue that will be created in this stack',
-      type: 'String'
-    });
-    const visibilityTimeout = new CfnParameter(this, 'VisibilityTimeout', {
-      description: 'The visibility timeout for the queue in seconds',
-      type: 'Number',
-      default: 30
-    });
+    const {
+      queueName,
+      visibilityTimeout
+    } = props;
     
     const queue = new sqs.Queue(this, 'SampleCdkSourceQueue', {
-      queueName: queueName.valueAsString,
-      visibilityTimeout: cdk.Duration.seconds(visibilityTimeout.valueAsNumber)
+      queueName: queueName,
+      visibilityTimeout: cdk.Duration.seconds(visibilityTimeout)
     });
   }
 }
